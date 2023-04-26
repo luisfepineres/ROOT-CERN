918740e992b948c727fe29e4a602f1cd4e57c893
luisfepineres <131925376+luisfepineres@users.noreply.github.com>
2023-04-27 01:41:30 +0200

    Libraries used in C++ and ROOT.

---
+++ physics_data_analysis.h
@@ -0,0 +1,23 @@
+#ifndef PHYSICS_DATA_ANALYSIS_H
+#define PHYSICS_DATA_ANALYSIS_H
+
+// Bibliotecas de C++
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <cmath>
+#include <algorithm>
+#include <ctime>
+// Bibliotecas de ROOT
+#include <TRandom3.h>
+#include <TFile.h>
+#include <TTree.h>
+#include <TH1.h>
+#include <TH2.h>
+#include <TCanvas.h>
+#include <TLegend.h>
+
+
+#endif // PHYSICS_DATA_ANALYSIS_H
